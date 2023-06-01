8b4ad0bbf6ccfe4344084fc4e853761dbc2ea90f
Trevor H <h.trevs@outlook.com>
2023-05-31 20:04:04 -0500

    Initial Spring Boot setup

---
+++ src/test/java/com/active/demo/ActiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.active.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
