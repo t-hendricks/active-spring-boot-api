936dcbdcead37020ff2da4cff940e60a1e1d7bbd
Trevor H <h.trevs@outlook.com>
2023-06-01 10:51:42 -0500

    Add static fields to keep test definitions DRY

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -5,11 +5,28 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ActiveApplication.class)
 public class SpringBootCucumberTestDefinitions {
+
+    private static final String BASE_URL = "http://localhost:";
+    private static final RequestSpecification request = RestAssured.given();
+    private static Response response;
+    private static String generalUserToken;
+
+    @LocalServerPort
+    String port;
+
+    public SpringBootCucumberTestDefinitions() {
+        RestAssured.baseURI = BASE_URL;
+    }
+
     @Given("A list of my posts exists")
     public void aListOfMyPostsExists() {
         
