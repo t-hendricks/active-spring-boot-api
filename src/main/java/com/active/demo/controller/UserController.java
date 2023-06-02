47330afef991690d6ca113c6aed87262416d2a4e
Trevor H <h.trevs@outlook.com>
2023-06-02 16:44:48 -0500

    Add POST endpoint to register a new user

--- src/main/java/com/active/demo/controller/UserController.java
+++ src/main/java/com/active/demo/controller/UserController.java
@@ -1,9 +1,39 @@
 package com.active.demo.controller;
 
+import com.active.demo.exception.InformationExistException;
+import com.active.demo.model.User;
+import com.active.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(path = "/auth")
 public class UserController {
+    private UserService userService;
+    static HashMap<String , Object> message = new HashMap<>();
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createUser(@RequestBody User userObj) {
+        try {
+            User newUser = userService.createUser(userObj);
+            message.put("message","success");
+            message.put("data",newUser);
+            return new ResponseEntity<>(message, HttpStatus.CREATED);
+        } catch (InformationExistException e){
+            message.put("message",e.getMessage());
+            return new ResponseEntity<>(message, HttpStatus.CONFLICT);
+        }
+    }
 }
