02be3b5f26295b7e207015f5c6297c6bcd668768
Trevor H <h.trevs@outlook.com>
2023-06-02 14:08:12 -0500

    Add custom query to find all user specific activities

--- src/main/java/com/active/demo/repository/ActivityRepository.java
+++ src/main/java/com/active/demo/repository/ActivityRepository.java
@@ -4,6 +4,9 @@ import com.active.demo.model.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    public List<Activity> findAllByOrderByUserIdAsc();
 }
