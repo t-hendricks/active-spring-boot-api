0ed2f02aa5e9f8073ebb0ffdbb7c0c7023ebf9ad
Trevor H <h.trevs@outlook.com>
2023-06-02 16:45:26 -0500

    Add create new user business logic

--- src/main/java/com/active/demo/service/UserService.java
+++ src/main/java/com/active/demo/service/UserService.java
@@ -1,7 +1,28 @@
 package com.active.demo.service;
 
+import com.active.demo.exception.InformationExistException;
+import com.active.demo.model.User;
+import com.active.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository/*, @Lazy PasswordEncoder passwordEncoder*/) {
+        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(User userObj) {
+        if(!userRepository.existsByUserName(userObj.getUserName())) {
+//            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObj);
+        } else {
+            throw new InformationExistException("User with username: " + userObj.getUserName() + " exists");
+        }
+    }
 }
