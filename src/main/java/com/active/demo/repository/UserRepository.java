4150dfd0099fb66ce5a19dcf323e12e837494058
Trevor H <h.trevs@outlook.com>
2023-06-02 09:58:37 -0500

    Create User Repository

---
+++ src/main/java/com/active/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.active.demo.repository;
+
+import com.active.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
