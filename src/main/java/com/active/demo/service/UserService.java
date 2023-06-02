f75af3e79c5e84491e6892ea71e1f2dbfc1b2d87
Trevor H <h.trevs@outlook.com>
2023-06-02 17:26:27 -0500

    Add spring security dependency and password encoder

--- src/main/java/com/active/demo/service/UserService.java
+++ src/main/java/com/active/demo/service/UserService.java
@@ -4,22 +4,24 @@ import com.active.demo.exception.InformationExistException;
 import com.active.demo.model.User;
 import com.active.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-//    private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository/*, @Lazy PasswordEncoder passwordEncoder*/) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-//        this.passwordEncoder = passwordEncoder;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User createUser(User userObj) {
         if(!userRepository.existsByUserName(userObj.getUserName())) {
-//            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            userObj.setPassword(passwordEncoder.encode(userObj.getPassword()));
             return userRepository.save(userObj);
         } else {
             throw new InformationExistException("User with username: " + userObj.getUserName() + " exists");
