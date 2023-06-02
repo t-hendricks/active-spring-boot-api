8e913d01b2dcaf9e00c06165ef5fc7e23295b46e
Trevor H <h.trevs@outlook.com>
2023-06-02 10:53:23 -0500

    Add failing test for create Activity POST endpoint

--- src/test/java/controller/ActivityControllerTest.java
+++ src/test/java/controller/ActivityControllerTest.java
@@ -1,12 +1,27 @@
 package controller;
 
 import com.active.demo.controller.ActivityController;
+import com.active.demo.model.Activity;
 import com.active.demo.service.ActivityService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ActivityController.class)
 public class ActivityControllerTest {
@@ -18,4 +33,30 @@ public class ActivityControllerTest {
 
     @MockBean
     private ActivityService activityService;
+
+    SimpleDateFormat form = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+    LocalDateTime now = LocalDateTime.now();
+
+    // Dummy data for test methods
+    Activity RECORD_1 = new Activity(1L, form.format(now), "Content 1");
+    Activity RECORD_2 = new Activity(2L, form.format(now), "Content 2");
+    Activity RECORD_3 = new Activity(3L, form.format(now), "Content 3");
+
+    @Test
+    public void createActivity_success() throws Exception {
+        when(activityService.createActivity(Mockito.any(Activity.class))).thenReturn(RECORD_1);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/activities")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(RECORD_1)); // serialization: convert from JSON - String
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.data.id").value(RECORD_1.getId()))
+                .andExpect(jsonPath("$.data.activityDate").value(RECORD_1.getActivityDate()))
+                .andExpect(jsonPath("$.data.content").value(RECORD_1.getContent()))
+                .andExpect(jsonPath("$.message").value("success"))
+                .andDo(print());
+    }
 }
