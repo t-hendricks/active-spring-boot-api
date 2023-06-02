921a628d2673fa0d64efcb3a9461f1beadf65577
Trevor H <h.trevs@outlook.com>
2023-06-02 11:57:44 -0500

    Add createActivity method business logic to pass createActivity_success test

--- src/main/java/com/active/demo/service/ActivityService.java
+++ src/main/java/com/active/demo/service/ActivityService.java
@@ -1,7 +1,20 @@
 package com.active.demo.service;
 
+import com.active.demo.model.Activity;
+import com.active.demo.repository.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ActivityService {
+    private ActivityRepository activityRepository;
+
+    @Autowired
+    public void setActivityRepository(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
+
+    public Activity createActivity(Activity activityObj) {
+        return activityRepository.save(activityObj);
+    }
 }
