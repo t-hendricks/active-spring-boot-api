61efdf4580faf0354a0e0cd217113fb37d5ac0f3
Trevor H <h.trevs@outlook.com>
2023-06-02 09:59:31 -0500

    Create Activity Repository

---
+++ src/main/java/com/active/demo/repository/ActivityRepository.java
@@ -0,0 +1,9 @@
+package com.active.demo.repository;
+
+import com.active.demo.model.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+}
