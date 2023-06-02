1901fc6cc01f27dd74fd60b285f57050b08d101a
Trevor H <h.trevs@outlook.com>
2023-06-02 10:18:47 -0500

    Create UserController Test

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -1,2 +1,21 @@
-package controller;public class UserControllerTest {
+package controller;
+
+import com.active.demo.controller.UserController;
+import com.active.demo.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+    @Autowired // dependency injection
+    private MockMvc mockMvc; // Java Bean
+
+    @Autowired
+    ObjectMapper mapper; // serialization: maps object to model and vice versa
+
+    @MockBean
+    private UserService userService;
 }
