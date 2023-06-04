6df3a3c5f45f9537df847d5de3ab0637c0b2723a
Trevor H <h.trevs@outlook.com>
2023-06-04 13:17:31 -0500

    Create Login Request

---
+++ src/main/java/com/active/demo/model/request/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.active.demo.model.request;
+
+public class LoginRequest {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
