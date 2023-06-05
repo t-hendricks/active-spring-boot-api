5f46a0ef1457bafe24f30da2f543c1bed3876578
Trevor H <h.trevs@outlook.com>
2023-06-05 10:57:38 -0500

    Add two step definitions to test registration

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -8,8 +8,12 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.Map;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ActiveApplication.class)
@@ -27,27 +31,22 @@ public class SpringBootCucumberTestDefinitions {
         RestAssured.baseURI = BASE_URL;
     }
 
-    @Given("A list of my posts exists")
-    public void aListOfMyPostsExists() {
-        
+    @When("User registers a unique account")
+    public void userRegistersAUniqueAccount() throws Exception {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("username", "JohnDoe");
+        requestBody.put("password", "12345");
+        request.header("Content-Type", "application/json");
+        response = request.body(requestBody.toString()).post(BASE_URL + port + "/auth/users/login");
     }
 
-    @When("I create a new post")
-    public void iCreateANewPost() {
-        
-    }
-
-    @Then("The post is created")
-    public void thePostIsCreated() {
-        
-    }
-
-    @When("I update any of my posts")
-    public void iUpdateAnyOfMyPosts() {
-        
-    }
-
-    @Then("The post is updated")
-    public void thePostIsUpdated() {
+    @Then("An account is created")
+    public void anAccountIsCreated() {
+        String msg = response.jsonPath().get("message");
+        Map<String, String> user = response.jsonPath().get("data");
+        Assert.assertEquals(201, response.getStatusCode());
+        Assert.assertEquals("success", msg);
+        Assert.assertEquals("JognDoe", user.get("username"));
+        Assert.assertEquals("12345", user.get("password"));
     }
 }
