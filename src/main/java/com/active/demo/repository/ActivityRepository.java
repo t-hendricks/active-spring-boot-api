39aabed56c8bdab4cc5fea1b702795489cabd5e6
Trevor H <h.trevs@outlook.com>
2023-06-05 09:07:50 -0500

    Add custom query to find any existing activity by id

--- src/main/java/com/active/demo/repository/ActivityRepository.java
+++ src/main/java/com/active/demo/repository/ActivityRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
     public List<Activity> findByUserIdOrderByIdAsc(Long userId);
     public Activity findByIdAndUserId(Long id, Long userId);
+    public Optional<Activity> findById(Long id);
 }
