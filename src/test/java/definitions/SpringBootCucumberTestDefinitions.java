b108c751439a10d0b7f81c48639234c92fff837d
Trevor H <h.trevs@outlook.com>
2023-06-05 12:08:47 -0500

    Add new scenario and step definitions for user to create and update new activity

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -13,6 +13,8 @@ import org.junit.Assert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @CucumberContextConfiguration
@@ -22,7 +24,7 @@ public class SpringBootCucumberTestDefinitions {
     private static final String BASE_URL = "http://localhost:";
     private static final RequestSpecification request = RestAssured.given();
     private static Response response;
-    private static String generalUserToken;
+    private static String userToken;
 
     @LocalServerPort
     String port;
@@ -34,10 +36,10 @@ public class SpringBootCucumberTestDefinitions {
     @When("User registers a unique account")
     public void userRegistersAUniqueAccount() throws Exception {
         JSONObject requestBody = new JSONObject();
-        requestBody.put("username", "JohnDoe");
+        requestBody.put("userName", "JohnDoe");
         requestBody.put("password", "12345");
         request.header("Content-Type", "application/json");
-        response = request.body(requestBody.toString()).post(BASE_URL + port + "/auth/users/login");
+        response = request.body(requestBody.toString()).post(BASE_URL + port + "/auth/users/register");
     }
 
     @Then("An account is created")
@@ -46,7 +48,64 @@ public class SpringBootCucumberTestDefinitions {
         Map<String, String> user = response.jsonPath().get("data");
         Assert.assertEquals(201, response.getStatusCode());
         Assert.assertEquals("success", msg);
-        Assert.assertEquals("JognDoe", user.get("username"));
-        Assert.assertEquals("12345", user.get("password"));
+        Assert.assertEquals("JohnDoe", user.get("userName"));
+    }
+
+    @When("User log in with credentials")
+    public void userLogInWithCredentials() throws Exception {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("userName", "JohnDoe");
+        requestBody.put("password", "12345");
+        request.header("Content-Type", "application/json");
+        response = request.body(requestBody.toString()).post(BASE_URL + port + "/auth/users/login");
+    }
+
+    @Then("The user is logged in")
+    public void theUserIsLoggedIn() {
+        String token = response.jsonPath().get("message");
+        Assert.assertEquals(200, response.getStatusCode());
+        Assert.assertEquals(String.class, token.getClass());
+        userToken = token;
+    }
+
+    @When("I create a new post")
+    public void iCreateANewPost() throws Exception {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("content", "content goes here");
+        request.header("Content-Type", "application/json");
+        request.header("Authorization", "Bearer " + userToken);
+        response = request.body(requestBody.toString()).post(BASE_URL + port + "/api/activities");
+    }
+
+    @Then("The post is created")
+    public void thePostIsCreated() {
+        Map<String, String> activity = response.jsonPath().get("data");
+        String msg = response.jsonPath().get("message");
+        Assert.assertEquals(201, response.getStatusCode());
+        Assert.assertEquals("content goes here", activity.get("content"));
+        Assert.assertEquals("success", msg);
+    }
+
+    @When("I update any of my posts")
+    public void iUpdateAnyOfMyPosts() throws Exception {
+        JSONObject requestBody = new JSONObject();
+        Map<String, String> activity = response.jsonPath().get("data");
+        String activityId = String.valueOf(activity.get("id"));
+        requestBody.put("content", "updated content");
+        request.header("Content-Type", "application/json");
+        request.header("Authorization", "Bearer " + userToken);
+        response = request.body(requestBody.toString()).put(BASE_URL + port + "/api/activities/" + activityId);
+    }
+
+    @Then("The post is updated")
+    public void thePostIsUpdated() {
+        Map<String, String> activity = response.jsonPath().get("data");
+        String msg = response.jsonPath().get("message");
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm a");
+        Assert.assertEquals(202, response.getStatusCode());
+        Assert.assertEquals("updated content", activity.get("content"));
+        Assert.assertEquals(now.format(formatter), activity.get("activityDate"));
+        Assert.assertEquals("success", msg);
     }
 }
