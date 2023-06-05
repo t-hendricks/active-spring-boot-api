5ba663e566ee3a7ecc33490941cd65907262220b
Trevor H <h.trevs@outlook.com>
2023-06-04 23:18:27 -0500

    Add a new exception for non existing info

--- src/main/java/com/active/demo/exception/InformationNotFoundException.java
+++ src/main/java/com/active/demo/exception/InformationNotFoundException.java
@@ -1,2 +1,11 @@
-package com.active.demo.exception;public class InformationNotFoundException {
+package com.active.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InformationNotFoundException extends RuntimeException {
+    public InformationNotFoundException(String message) {
+        super(message);
+    }
 }
