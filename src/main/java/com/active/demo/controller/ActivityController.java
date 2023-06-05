69d433b24c7d3146fd0ab3ff160dee769e5d76dc
Trevor H <h.trevs@outlook.com>
2023-06-04 23:16:54 -0500

    Add PUT mapping to update an activity

--- src/main/java/com/active/demo/controller/ActivityController.java
+++ src/main/java/com/active/demo/controller/ActivityController.java
@@ -5,10 +5,7 @@ import com.active.demo.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -30,6 +27,19 @@ public class ActivityController {
         } else {
             message.put("message", "unable to create an activity at this time");
             return new ResponseEntity<>(message, HttpStatus.TOO_EARLY);
+        }
+    }
+
+    @PutMapping("/activities/{activityId}")
+    public ResponseEntity<?> updateActivity(@PathVariable Long activityId, @RequestBody Activity activityObj) {
+        Activity updatedActivity = activityService.updateActivity(activityId, activityObj);
+        if (updatedActivity != null) {
+            message.put("message", "success");
+            message.put("data", updatedActivity);
+            return new ResponseEntity<>(message, HttpStatus.ACCEPTED);
+        } else {
+            message.put("message", "cannot find activity with id " + activityId);
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
         }
     }
 }
