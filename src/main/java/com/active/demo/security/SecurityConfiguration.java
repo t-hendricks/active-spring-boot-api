f11f8a7d512e8ed75e9dc58eb782dba0362277b4
Trevor H <h.trevs@outlook.com>
2023-06-05 15:33:48 -0500

    Add CORS bean to enable cross-origin resource sharing for Angular

--- src/main/java/com/active/demo/security/SecurityConfiguration.java
+++ src/main/java/com/active/demo/security/SecurityConfiguration.java
@@ -16,6 +16,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -25,6 +27,19 @@ public class SecurityConfiguration {
     @Autowired
     public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
         this.myUserDetailsService = myUserDetailsService;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 
     @Bean
@@ -49,6 +64,7 @@ public class SecurityConfiguration {
                 .and().csrf().disable();
         http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class); // added for JWT login
         http.headers().frameOptions().disable(); // needed to see h2 database after login
+        http.cors();
         return http.build();
     }
 
