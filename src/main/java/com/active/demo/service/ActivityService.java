20fac652cd2bece6e68ee4ce680c0f2a4b804a3b
Trevor H <h.trevs@outlook.com>
2023-06-04 22:01:15 -0500

    Update createActivity method to handle date checking

--- src/main/java/com/active/demo/service/ActivityService.java
+++ src/main/java/com/active/demo/service/ActivityService.java
@@ -1,5 +1,7 @@
 package com.active.demo.service;
 
+import com.active.demo.exception.InformationExistException;
+import com.active.demo.exception.RepeatingException;
 import com.active.demo.model.Activity;
 import com.active.demo.model.User;
 import com.active.demo.repository.ActivityRepository;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Service
 public class ActivityService {
@@ -26,12 +29,27 @@ public class ActivityService {
         return userDetails.getUser();
     }
 
+    public boolean isPostTooEarly(LocalDateTime now) {
+        List<Activity> userActivities = activityRepository.findByUserIdOrderByIdAsc(getCurrentLoggedInUser().getId());
+        if (userActivities.size() == 0) return false;
+
+        Activity latestPost = userActivities.get(userActivities.size() - 1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm a");
+        LocalDateTime latestDate = LocalDateTime.parse(latestPost.getActivityDate(), formatter);
+
+        return latestDate.isBefore(now.minusMinutes(1)) ? false : true;
+    }
+
     public Activity createActivity(Activity activityObj) {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm a");
 
-        activityObj.setUser(getCurrentLoggedInUser());
-        activityObj.setActivityDate(now.format(formatter));
+        if (!isPostTooEarly(now)) {
+            activityObj.setUser(getCurrentLoggedInUser());
+            activityObj.setActivityDate(now.format(formatter));
+        } else {
+            throw new RepeatingException("Cannot post less than 24 hours from your last post");
+        }
 
         return activityRepository.save(activityObj);
     }
